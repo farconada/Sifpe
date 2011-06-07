edda17089dd9870da0ccc2e277e146ddadf7f654
Fernando Arconada <fernando.arconada@gmail.com>
2011-06-07 22:29:41 +0200

    Controlador gasto e ingreso y templates para gasto

---
+++ Classes/Domain/Repository/GastoRepository.php
@@ -0,0 +1,14 @@
+<?php
+declare(ENCODING = 'utf-8');
+namespace F3\Sifpe\Domain\Repository;
+ 
+/**
+ * GastoRepository
+ *
+ * 
+ *
+ */
+  
+class GastoRepository extends \F3\FLOW3\Persistence\Repository {
+ 
+}
---
+++ Classes/Domain/Repository/IngresoRepository.php
@@ -0,0 +1,14 @@
+<?php
+declare(ENCODING = 'utf-8');
+namespace F3\Sifpe\Domain\Repository;
+ 
+/**
+ * IngresoRepository
+ *
+ * 
+ *
+ */
+  
+class IngresoRepository extends \F3\FLOW3\Persistence\Repository {
+ 
+}
