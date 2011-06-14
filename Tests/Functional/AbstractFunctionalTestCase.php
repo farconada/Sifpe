a1335047f2a1bc1b926b34511a0519354f308700
farconada <fernando.arconada@gmail.com>
2011-06-14 14:29:52 +0200

    test funcionales

--- Tests/Functional/AbstractFunctionalTestCase.php
+++ Tests/Functional/AbstractFunctionalTestCase.php
@@ -1,12 +1,13 @@
 <?php
-declare(ENCODING = 'utf-8');
+declare(ENCODING = 'utf-8') ;
 namespace F3\Sifpe\Tests\Functional;
 /**
  * Clase abstracta para cargar fixtures en la BD
  * @author Fernando Arconada fernando.arconada@gmail.com
  * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License, version 3 or later
  */
-class AbstractFunctionalTestCase extends \F3\FLOW3\Tests\FunctionalTestCase {
+class AbstractFunctionalTestCase extends \F3\FLOW3\Tests\FunctionalTestCase
+{
     public function setUp()
     {
         parent::setUp();
@@ -15,17 +16,20 @@ class AbstractFunctionalTestCase extends \F3\FLOW3\Tests\FunctionalTestCase {
 
         $entityManagerFactory = $this->objectManager->get('\F3\FLOW3\Persistence\Doctrine\EntityManagerFactory');
         $entityManager = $entityManagerFactory->create();
-        $fixturesSqlFile = $settings['FLOW3']['persistence']['backendOptions']['fixtures'];
-        if(is_readable($fixturesSqlFile)) {
-            $sql = file_get_contents($fixturesSqlFile);
-            $connection = $entityManager->getConnection();
-            try{
-                $connection->exec($sql);
-            }catch (\PDOException $e){
-               echo  $e->getMessage(). PHP_EOL;
-               echo  $e->getTraceAsString() . PHP_EOL;
+        if (isset($settings['FLOW3']['persistence']['backendOptions']['fixtures'])) {
+            $fixturesSqlFile = $settings['FLOW3']['persistence']['backendOptions']['fixtures'];
+            if (is_readable($fixturesSqlFile)) {
+                $sql = file_get_contents($fixturesSqlFile);
+                $connection = $entityManager->getConnection();
+                try {
+                    $connection->exec($sql);
+                } catch (\PDOException $e) {
+                    echo  $e->getMessage() . PHP_EOL;
+                    echo  $e->getTraceAsString() . PHP_EOL;
+                }
             }
         }
+
     }
 
     static public function tearDownAfterClass()
--- Tests/Functional/Property/JsonProperty.php
+++ Tests/Functional/Property/JsonProperty.php
@@ -1,48 +1,78 @@
 <?php
-declare(ENCODING = 'utf-8');
+declare(ENCODING = 'utf-8') ;
 namespace F3\Sifpe\Tests\Functional\Property;
 /**
  * Testcase for Property Mapper
  * @author Fernando Arconada fernando.arconada@gmail.com
  * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License, version 3 or later
  */
-class JsonProperty extends \F3\Sifpe\Tests\Functional\AbstractFunctionalTestCase {
+class JsonProperty extends \F3\Sifpe\Tests\Functional\AbstractFunctionalTestCase
+{
     /**
-	 *
-	 * @var \F3\Sifpe\TypeConverters\JsonToEntityConverter
-	 */
-	protected $typeConverter;
+     *
+     * @var \F3\Sifpe\TypeConverters\JsonToEntityConverter
+     */
+    protected $typeConverter;
 
     /**
-	 * @var boolean
-	 */
-	static protected $testablePersistenceEnabled = TRUE;
+     * @inject
+     * @var \F3\Sifpe\Domain\Repository\EmpresaRepository
+     */
+    protected $empresaRepository;
 
-	/**
-	 * @return void
-	 */
-	public function setUp() {
-		parent::setUp();
-		$this->typeConverter = $this->objectManager->get('\F3\Sifpe\TypeConverters\JsonToEntityConverter');
-	}
+    /**
+     * @var boolean
+     */
+    static protected $testablePersistenceEnabled = TRUE;
+
+    /**
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $this->empresaRepository = new \F3\Sifpe\Domain\Repository\EmpresaRepository();
+        $this->typeConverter = $this->objectManager->get('\F3\Sifpe\TypeConverters\JsonToEntityConverter');
+    }
 
     /**
      * @test
      * @dataProvider empresasJson
      * @return void
      */
-    public function convertToEmpresa($jsonParam) {
-       $convertedObject = $this->typeConverter->convertFrom($jsonParam,'\F3\Sifpe\Domain\Model\Empresa');
-       $this->assertTrue(is_object($convertedObject));
+    public function convertToEmpresa($jsonParam)
+    {
+        $convertedObject = $this->typeConverter->convertFrom($jsonParam, '\F3\Sifpe\Domain\Model\Empresa');
+        $this->assertTrue(is_object($convertedObject));
     }
 
-    public function empresasJson() {
+    /**
+     * @test
+     * @dataProvider empresasJson
+     * @param  $jsonParam
+     * @return void
+     */
+    public function deleteAllEmpresas($jsonParam) {
+        $convertedObject = $this->typeConverter->convertFrom($jsonParam, '\F3\Sifpe\Domain\Model\Empresa');
+        $empresasAntes = $this->empresaRepository->findAll()->count();
+        $output = $this->sendWebRequest('Empresa','Sifpe','delete',array('entity' =>$convertedObject),'json');
+        $empresasDespues = $this->empresaRepository->findAll()->count();
+        $this->assertEquals($empresasAntes-1,$empresasDespues);
+    }
+
+    public function empresasJson()
+    {
         $empresasJsonArray = array();
-        $empresasJsonArray[] = array(json_encode(array(
-                                              'id'      => 1,
-                                              'name'    => 'Fernando'
-                                           ))
-        );
+        $empresaRespository = new \F3\Sifpe\Domain\Repository\EmpresaRepository();
+        $empresas = $empresaRespository->findAll();
+        foreach ($empresas as $empresa) {
+            $empresasJsonArray[] = array(json_encode(array(
+                                                          'id' => $empresa->getId(),
+                                                          'name' => $empresa->getName()
+                                                     ))
+            );
+        }
+
         return $empresasJsonArray;
     }
 }
