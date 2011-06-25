bbb76fa419da0aba3f66780856eb39742f95e208
Fernando Arconada <fernando.arconada@gmail.com>
2011-06-25 18:19:26 +0200

    inicio de backupController

---
+++ Classes/Controller/BackupController.php
@@ -0,0 +1,19 @@
+<?php
+declare(ENCODING = 'utf-8');
+namespace F3\Sifpe\Controller;
+ 
+/*
+ * BackupController
+ *
+ * Gestion de Backups
+ */
+class BackupController extends \F3\FLOW3\MVC\Controller\ActionController {
+ 
+    /**
+     * indexAction
+     * @return void
+     */
+    public function indexAction() {
+    }
+ 
+}
