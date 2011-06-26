acc3260b7895fae9c781fb9914ced18eaab6922b
Fernando Arconada <fernando.arconada@gmail.com>
2011-06-26 18:41:56 +0200

    modulo para hacer backup

--- Classes/Controller/BackupController.php
+++ Classes/Controller/BackupController.php
@@ -14,6 +14,24 @@ class BackupController extends \F3\FLOW3\MVC\Controller\ActionController {
      * @return void
      */
     public function indexAction() {
+        $iterator = new \DirectoryIterator($this->settings['backupDir']);
+        $files = array();
+        $i = 0;
+        foreach ($iterator as $fileinfo) {
+            if ($fileinfo->isFile() && preg_match('/backup-.*/',$fileinfo->getFilename())) {
+                $files[$i]['filename'] = $fileinfo->getFilename();
+                $files[$i]['ctime'] = $fileinfo->getCTime();
+                $i++;
+            }
+        }
+        $this->view->assign('files',$files);
+    }
+
+    public function doBackupAction(){
+        //Solo es valido para MySQL
+        $date = new \DateTime();
+        $cmd = '/usr/bin/mysqldump -uroot flow3 >'.$this->settings['backupDir'].'backup-'.$date->format('Ymd').'.sql';
+        system('echo "'.$cmd.'"| at now +1min');
     }
  
 }
