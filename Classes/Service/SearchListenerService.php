0e0c4ccc2d1a857bee721830fcedbe36d477811c
Fernando Arconada <fernando.arconada@gmail.com>
2011-06-27 22:27:36 +0200

    removed ZF libs

--- Classes/Service/SearchListenerService.php
+++ Classes/Service/SearchListenerService.php
@@ -1,6 +1,10 @@
 <?php
 declare(ENCODING = 'utf-8') ;
 namespace F3\Sifpe\Service;
+require_once 'Zend/Search/Lucene.php';
+require_once 'Zend/Search/Lucene/Field.php';
+require_once 'Zend/Search/Lucene/Document.php';
+require_once 'Zend/Search/Lucene/Exception.php';
 
 /**
  * @scope singleton
@@ -26,9 +30,9 @@ class SearchListenerService implements DoctrineEventListenerInterface
             throw new \F3\FLOW3\Error\Exception('Falta la configuracion del directorio de Lucene o el directorio no se puede leer', 1309201437);
         }
         try {
-            $this->index = \Zend_Search_Lucene::open($this->settings['luceneDir']);
-        } catch (\Zend_Search_Lucene_Exception $ex) {
-            $this->index = \Zend_Search_Lucene::create($this->settings['luceneDir']);
+            $this->index = Zend_Search_Lucene::open($this->settings['luceneDir']);
+        } catch (Zend_Search_Lucene_Exception $ex) {
+            $this->index = Zend_Search_Lucene::create($this->settings['luceneDir']);
         }
 
     }
@@ -40,14 +44,14 @@ class SearchListenerService implements DoctrineEventListenerInterface
             
             $this->deleteApunteIndex($apunte);
             
-            $doc = new \Zend_Search_Lucene_Document();
-            $doc->addField(\Zend_Search_Lucene_Field::Keyword('id', $apunte->getId()));
-            $doc->addField(\Zend_Search_Lucene_Field::Keyword('class', get_class($apunte)));
-            $doc->addField(\Zend_Search_Lucene_Field::Text('empresa', $apunte->getEmpresa()->getName()));
-            $doc->addField(\Zend_Search_Lucene_Field::Text('cuenta', $apunte->getCuenta()->getName()));
-            $doc->addField(\Zend_Search_Lucene_Field::UnStored('notas', $apunte->getNotas()));
-            $doc->addField(\Zend_Search_Lucene_Field::UnIndexed('cantidad', $apunte->getCantidad()));
-            $doc->addField(\Zend_Search_Lucene_Field::UnIndexed('fecha', $apunte->getFecha()->format('Ymd')));
+            $doc = new Zend_Search_Lucene_Document();
+            $doc->addField(Zend_Search_Lucene_Field::Keyword('id', $apunte->getId()));
+            $doc->addField(Zend_Search_Lucene_Field::Keyword('class', get_class($apunte)));
+            $doc->addField(Zend_Search_Lucene_Field::Text('empresa', $apunte->getEmpresa()->getName()));
+            $doc->addField(Zend_Search_Lucene_Field::Text('cuenta', $apunte->getCuenta()->getName()));
+            $doc->addField(Zend_Search_Lucene_Field::UnStored('notas', $apunte->getNotas()));
+            $doc->addField(Zend_Search_Lucene_Field::UnIndexed('cantidad', $apunte->getCantidad()));
+            $doc->addField(Zend_Search_Lucene_Field::UnIndexed('fecha', $apunte->getFecha()->format('Ymd')));
             $this->index->addDocument($doc);
         }
     }
