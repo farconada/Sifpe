de684d6ff33403089fbf72fa95999a9563962e99
Fernando Arconada <fernando.arconada@gmail.com>
2011-06-27 19:35:06 +0200

    Refactorizado search listener para insertarlo con DI y una interfact de doctrineEventListener

--- Classes/Controller/ApunteController.php
+++ Classes/Controller/ApunteController.php
@@ -12,15 +12,19 @@ namespace F3\Sifpe\Controller;
  */
 class ApunteController extends AbstractController
 {
+    /**
+     * @var \F3\Sifpe\Service\DoctrineEventListenerInterface
+     */
+    protected $doctrineEventListener;
+
     protected function initializeAction()
     {
         parent::initializeAction();
 
-        $searchListener = $this->objectManager->get('\F3\Sifpe\Service\SearchListenerService');
         $entityManagerFactory = $this->objectManager->get('\F3\FLOW3\Persistence\Doctrine\EntityManagerFactory');
         $entityManager = $entityManagerFactory->create();
         $entityManager->getEventManager()->addEventListener(
-            array(\Doctrine\ORM\Events::preUpdate, \Doctrine\ORM\Events::preRemove), $searchListener
+            array(\Doctrine\ORM\Events::preUpdate, \Doctrine\ORM\Events::preRemove), $this->doctrineEventListener
         );
         $this->persistenceManager->injectEntityManager($entityManager);
 
--- Classes/Service/SearchListenerService.php
+++ Classes/Service/SearchListenerService.php
@@ -2,7 +2,7 @@
 declare(ENCODING = 'utf-8') ;
 namespace F3\Sifpe\Service;
 
-class SearchListenerService
+class SearchListenerService implements DoctrineEventListenerInterface
 {
 
     public function preUpdate(\Doctrine\ORM\Event\LifecycleEventArgs $args)
